4cba48bd66dc1881ff304def98e4b215bd246226
vcher <vchernesky@gmail.com>
2019-11-25 05:48:05 -0500

    Updated Arm Encoder Test to see what happens if you just set power to zero if the arm is at the desired position. (Trying to get rid of the jerking behaviou when stoppped.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmEncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmEncoderTest.java
@@ -37,82 +37,33 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import static java.lang.Math.abs;
 
-/**
- * This OpMode uses the common Pushbot hardware class to define the devices on the robot.
- * All device access is managed through the HardwarePushbot class.
- * The code is structured as a LinearOpMode
- *
- * This particular OpMode executes a POV Game style Teleop for a PushBot
- * In this mode the left stick moves the robot FWD and back, the Right stick turns left and right.
- * It raises and lowers the claw using the Gampad Y and A buttons respectively.
- * It also opens and closes the claws slowly using the left and right Bumper buttons.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-
 //the method that defines all of the variables in this program and translates the gamepad inputs to robot outputs
 @TeleOp(name="ArmEncoderTest", group="Pushbot")
 public class ArmEncoderTest extends LinearOpMode {
 
     /* Declare OpMode members. */
     HardwareBruinBot robot           = new HardwareBruinBot();   // Use a Pushbot's hardware
-    //double          clawOffset      = 0;                       // Servo mid position
-    //final double    CLAW_SPEED      = 0.02 ;                   // sets rate to move servo
-
     static final double     COUNTS_PER_MOTOR_REV    = 360 ;
 
     @Override
     public void runOpMode() {
-//        double left;
-//        double right;
-//        double drive;
-//        double turn;
-//        double max;
 
-        float drive = 0;
-        float strafe = 0;
-        float rotate = 0;
-        float rampUp = 0;
-        float rampDown = 0;
-        float clawOpen = 0;
-        float clawClose = 0;
-//
-        boolean lowerArmMotorUp = false;
-        boolean lowerArmMotorDown = false;
-        boolean intakeFront = false;
-        boolean intakeBack = false;
-        boolean upperArmMotorOut = false;
-        boolean upperArmMotorIn = false;
 
-        boolean clawRest = true;
-        boolean capstoneOut = false;
-        boolean capstoneIn = false;
-        boolean armOut = false;
-        boolean armIn = false;
         boolean armUp = false;
         boolean armDown = false;
-        boolean platformServoDown = false;
-        boolean platformServoUp = false;
-
 
         int currentArmLiftPosition =0;  // Used to store the currently commanded arm position
-        int MAX_LIFTARM_POSITION = 70;  // ABout 70 steps from arm starting position to full extension
+        int MAX_LIFTARM_POSITION = 400;  // ABout 70 steps from arm starting position to full extension
+        int MIN_LIFTARM_POSITION = -400;
 
-        ElapsedTime runtime = new ElapsedTime();
 
-        waitForStart();
+
 
         /* Initialize the hardware variables.
          * The init() method of the hardware class does all the work here
          */
         robot.init(hardwareMap);
         robot.armLiftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-
-        // Send telemetry message to signify robot waiting;
-        telemetry.addData("Say", "Hello Driver");    //
-        telemetry.update();
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -131,21 +82,30 @@ public class ArmEncoderTest extends LinearOpMode {
                     currentArmLiftPosition += 10; // Add 10 to the current arm position
                     if (currentArmLiftPosition > MAX_LIFTARM_POSITION) {
                         currentArmLiftPosition = MAX_LIFTARM_POSITION; // DOn't let it go highter than Max Position
+                        robot.armLiftMotor.setTargetPosition(currentArmLiftPosition);
+                        robot.armLiftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        robot.armLiftMotor.setPower(.2);
                     }
                 } else {
                     if (armDown) {
                         currentArmLiftPosition -= 10; // Subtract 10 from the current arm position
-                        if (currentArmLiftPosition < 0) {
-                            currentArmLiftPosition = 0;  // Don't let it go lower than 0
+                        if (currentArmLiftPosition < MIN_LIFTARM_POSITION) {
+                            currentArmLiftPosition = MIN_LIFTARM_POSITION;  // Don't let it go lower than 0
+                            robot.armLiftMotor.setTargetPosition(currentArmLiftPosition);
+                            robot.armLiftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                            robot.armLiftMotor.setPower(.2);
                         }
+                    }
+
+                else {
+                        robot.armLiftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                        robot.armLiftMotor.setPower(0);
                     }
                 }
             }
             telemetry.addData("Current Commanded Pos: ",currentArmLiftPosition);
             telemetry.addData("Actual Pos: ",robot.armLiftMotor.getCurrentPosition());
-            robot.armLiftMotor.setTargetPosition(currentArmLiftPosition);
-            robot.armLiftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.armLiftMotor.setPower(.2);
+
             telemetry.update();
         }
 
